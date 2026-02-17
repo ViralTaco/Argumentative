04e80b10602f0c9abc9c36d588bb6c22059a59c8
Anthony Capobianco <liq4tdot@duck.com>
2026-02-17 23:50:04 +0100

    MOD: moved code to inline v1 namespace ADD: mark for inline namespace MOD: Copyright and version number

--- include/argumentative/utils/swap_sign.hpp
+++ include/argumentative/utils/swap_sign.hpp
@@ -1,26 +1,26 @@
 #ifndef VT_SWAP_SIGN_HPP
-// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-// ┃ swap_sign.hpp:                                       ┃
-// ┃ Copyright (c) 2020 viraltaco_ (viraltaco@gmx.com)    ┃
-// ┃ https://github.com/ViralTaco                         ┃
-// ┃ SPDX-License-Identifier: MIT                         ┃
-// ┃ <http://www.opensource.org/licenses/MIT>             ┃
-// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-#define VT_SWAP_SIGN_HPP "1.0.1"
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ swap_sign.hpp:                                             ┃
+// ┃ Copyright (c) 2020, 2026 viraltaco_ (viraltaco@gmx.com)    ┃
+// ┃ https://github.com/ViralTaco                               ┃
+// ┃ SPDX-License-Identifier: MIT                               ┃
+// ┃ <http://www.opensource.org/licenses/MIT>                   ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+#define VT_SWAP_SIGN_HPP "1.1.0"
 
 #include <type_traits>
 
 namespace argumentative {
-
-template <class T> [[nodiscard, maybe_unused]]
-constexpr auto swap_sign(const T integral) noexcept {
-  if constexpr (std::is_unsigned_v<T>) { // unsigned --> signed
-    return static_cast< std::make_signed_t<T> > (integral);
-  } else {                               // signed --> unsigned
-    return static_cast< std::make_unsigned_t<T> > (integral);
+ inline namespace v1 { /// MARK: - v1
+    template <class T> [[nodiscard, maybe_unused]]
+    constexpr auto swap_sign(const T integral) noexcept {
+      if constexpr (std::is_unsigned_v<T>) { // unsigned --> signed
+        return static_cast< std::make_signed_t<T> > (integral);
+      } else {                               // signed --> unsigned
+      return static_cast< std::make_unsigned_t<T> > (integral);
+    }
   }
-}
-
-} namespace ive = argumentative;
-
+ } // argumentative::inline v1
+} // namespace argumentative
+namespace ive = argumentative::v2;
 #endif
